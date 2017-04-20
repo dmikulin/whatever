bbce2fba0640f6ed37377a9350932b55daf6e791
ninozvorc <nino.zvorc@foi.hr>
2017-04-20 23:44:59 +0200

    YaaS call commented

--- src/main/java/foi/core/whatever/controllers/NewProductController.java
+++ src/main/java/foi/core/whatever/controllers/NewProductController.java
@@ -1,17 +1,22 @@
 package foi.core.whatever.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -56,25 +61,25 @@ public class NewProductController {
 		product.setPriceUSD( Double.parseDouble(request.getParameter("price_USD")));
 		productService.save(product);
 		
-		String url = "https://api.yaas.io/hybris/product/v2/milena/products";
-		String token = "Bearer 022-c1169181-7cdd-4da1-936e-9d38797f6cb8";
-		
-		String json = "{\"name\":\""+product.getName()+"\",";
-		       json += "\"code\":\""+product.getProductNumber()+"\",";
-		       json += "\"description\":\""+product.getDescription()+"\"}";
-		
-		HttpClient client = HttpClientBuilder.create().build();
-		HttpPost post = new HttpPost(url);
-		
-		post.setHeader("Authorization", token);
-		post.setHeader("Content-Language", "en");
-		post.setHeader("Content-Type", "application/json");
-		
-        HttpEntity entity = new ByteArrayEntity(json.getBytes("UTF-8"));
-		post.setEntity(entity);
-		HttpResponse response = client.execute(post);
-		
-		System.out.println("Response status: "+  response.getStatusLine().getStatusCode());
+//		String url = "https://api.yaas.io/hybris/product/v2/milena/products";
+//		String token = "Bearer 022-c1169181-7cdd-4da1-936e-9d38797f6cb8";
+//		
+//		String json = "{\"name\":\""+product.getName()+"\",";
+//		       json += "\"code\":\""+product.getProductNumber()+"\",";
+//		       json += "\"description\":\""+product.getDescription()+"\"}";
+//		
+//		HttpClient client = HttpClientBuilder.create().build();
+//		HttpPost post = new HttpPost(url);
+//		
+//		post.setHeader("Authorization", token);
+//		post.setHeader("Content-Language", "en");
+//		post.setHeader("Content-Type", "application/json");
+//		
+//        HttpEntity entity = new ByteArrayEntity(json.getBytes("UTF-8"));
+//		post.setEntity(entity);
+//		HttpResponse response = client.execute(post);
+//		
+//		System.out.println("Response status: "+  response.getStatusLine().getStatusCode());
 		
 		return "redirect:product-list";
 	}
