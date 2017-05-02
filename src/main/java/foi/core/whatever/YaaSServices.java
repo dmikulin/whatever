02584ccbfb67db0ee3daf9799f29096bdf8216fc
ninozvorc <nino.zvorc@foi.hr>
2017-05-02 22:08:13 +0200

    YaaS: Get all users, edit user

--- src/main/java/foi/core/whatever/YaaSServices.java
+++ src/main/java/foi/core/whatever/YaaSServices.java
@@ -9,6 +9,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPatch;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -26,7 +27,7 @@ import foi.core.whatever.model.User;
 @Component
 public class YaaSServices {
 
-	private static String TOKEN = "Bearer 022-1545e603-cbde-40c5-882c-1452ff519a27";
+	private static String TOKEN = "Bearer 022-7c5c3b34-6200-4df1-a130-5b1c101eb339";
 
 	private static String PRODUCT_SERVICE = "https://api.yaas.io/hybris/product/v2/noviprojekt/products";
 	private static String PRODUCT_DETAILS_SERVICE = "https://api.beta.yaas.io/hybris/productdetails/v2/noviprojekt/productdetails";
@@ -154,8 +155,28 @@ public class YaaSServices {
 
 		return customerId;
 	}
+	
+	public void editCustomer(User user) throws ClientProtocolException, IOException{	
+		String json = "{\"firstName\":\"" + user.getFirstName() + "\",";
+		json += "\"lastName\":\"" + user.getLastName() + "\",";
+		json += "\"contactEmail\":\"" + user.getEmail() + "\",";
+		json += "\"contactPhone\":\"" + user.getPhone() + "\",";
+		json += "\"active\":"+ user.isActive()+"}";
 
-	public List<User> getAllUsers() throws ClientProtocolException, IOException{
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpPatch patch = new HttpPatch(CUSTOMER_SERVICE+"/"+user.getYaasId());
+
+		patch.setHeader("Authorization", TOKEN);
+		patch.setHeader("Content-Type", "application/json");
+
+		HttpEntity entity = new ByteArrayEntity(json.getBytes("UTF-8"));
+		patch.setEntity(entity);
+		HttpResponse response = client.execute(patch);
+
+		System.out.println("[EDIT CUSTOMER] Response Status: "+  response.getStatusLine().getStatusCode());
+	}
+
+	public List<User> getAllUsers() throws ClientProtocolException, IOException, JSONException{
 		HttpClient client = HttpClientBuilder.create().build();
 		HttpGet get = new HttpGet(CUSTOMER_SERVICE);
 
@@ -163,10 +184,23 @@ public class YaaSServices {
 		HttpResponse response = client.execute(get);
 
 		String jsonString = EntityUtils.toString(response.getEntity());
-
 		System.out.println("All customers: "+  jsonString);
+		
+		List<User> users = new ArrayList<>();
+		JSONArray jsonCustomers = new JSONArray(jsonString);
+		for (int i = 0; i < jsonCustomers.length(); i++) {
+			User user = new User();
+		    JSONObject jsonCustomer = jsonCustomers.getJSONObject(i);
+		    user.setFirstName(jsonCustomer.getString("firstName"));
+		    user.setLastName(jsonCustomer.getString("lastName"));
+		    user.setPhone(jsonCustomer.getString("contactPhone"));
+		    user.setEmail(jsonCustomer.getString("contactEmail"));
+		    user.setActive(jsonCustomer.getBoolean("active"));
+		    user.setYaasId(jsonCustomer.getString("id"));
+		    users.add(user);
+		}
 
-		return null;
+		return users;
 	}
 
 
--- src/main/java/foi/core/whatever/controllers/UserListController.java
+++ src/main/java/foi/core/whatever/controllers/UserListController.java
@@ -1,9 +1,12 @@
 package foi.core.whatever.controllers;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import foi.core.whatever.YaaSServices;
 import foi.core.whatever.model.User;
 import foi.core.whatever.services.UserService;
 
@@ -21,39 +25,53 @@ public class UserListController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private YaaSServices yaaSServices;
+	
 	@RequestMapping(value = "/user-list", method = RequestMethod.GET)
-	public String userListPage(HttpServletRequest request, Model model) {
+	public String userListPage(HttpServletRequest request, Model model) throws ClientProtocolException, IOException, JSONException {
 		
 		String active = request.getParameter("active");
 		if(active == null){
 			active = "1";
 		}
-
-		List<User> activeUsers = userService.findByActive(true);
-		List<User> deactiveUsers = userService.findByActive(false);
-		model.addAttribute("activeUsers", activeUsers);
-		model.addAttribute("deactiveUsers", deactiveUsers);
+		List<User> allUsers = yaaSServices.getAllUsers();
+		
+		for(User user: allUsers){
+			user.setAvatar(userService.findByEmail(user.getEmail()).getAvatar());
+			user.setUserId(userService.findByEmail(user.getEmail()).getUserId());
+		}
+		
+		//List<User> activeUsers = userService.findByActive(true);
+		//List<User> deactiveUsers = userService.findByActive(false);
+		//model.addAttribute("activeUsers", activeUsers);
+		//model.addAttribute("deactiveUsers", deactiveUsers);
+		model.addAttribute("allUsers", allUsers);
 		model.addAttribute("active", Integer.parseInt(active));
 		
 		return "user-list";	
 	}
 	
 	@RequestMapping(value = "/deactivate-user/{id}", method = RequestMethod.GET)
-	public String deactivateUser(HttpServletRequest request, Model model, @PathVariable("id") int userId) {
+	public String deactivateUser(HttpServletRequest request, Model model, @PathVariable("id") int userId) throws ClientProtocolException, IOException {
 		
 		User user = userService.findByUserId(userId);
 		user.setActive(false);
 		userService.save(user);
 		
+		yaaSServices.editCustomer(user);
+		
 		return "redirect:/admin/user-list/?active=2";
 	}
 	
 	@RequestMapping(value = "/activate-user/{id}", method = RequestMethod.GET)
-	public String activateUser(HttpServletRequest request, Model model, @PathVariable("id") int userId) {
+	public String activateUser(HttpServletRequest request, Model model, @PathVariable("id") int userId) throws ClientProtocolException, IOException {
 		
 		User user = userService.findByUserId(userId);
 		user.setActive(true);
 		userService.save(user);
+		
+		yaaSServices.editCustomer(user);
 		
 		return "redirect:/admin/user-list/?active=1";
 	}
